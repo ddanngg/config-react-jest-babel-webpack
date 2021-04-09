ebeeaacffe46512d4d236ae6180f35251e15c4b6
Nguyen An <an.nguyen@spiritlabs.co>
2021-04-09 16:16:44 +0700

    add.test: test snapshot render component

---
+++ src/shared/__tests__/calculator-display.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+
+import CalculatorDisplay from '../calculator-display'
+
+test('render', () => {
+  const {container} = render(<CalculatorDisplay value="0" />)
+  expect(container).toMatchInlineSnapshot(`
+    <div>
+      <div
+        class="css-12149uf"
+      >
+        <div
+          class="autoScalingText"
+          style="transform: scale(1,1);"
+        >
+          0
+        </div>
+      </div>
+    </div>
+  `)
+})
