2e5ec617f1cdec6cc16440dc54057da89214e938
Nguyen An <an.nguyen@spiritlabs.co>
2021-04-10 16:25:18 +0700

    add: test component with context provider

--- src/shared/__tests__/calculator-display.js
+++ src/shared/__tests__/calculator-display.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import {render} from '@testing-library/react'
+import {ThemeProvider} from '@emotion/react'
 
 import CalculatorDisplay from '../calculator-display'
 
+import {dark} from '../../themes'
+
 test('render', () => {
-  const {container} = render(<CalculatorDisplay value="0" />)
+  const {container} = render(
+    <ThemeProvider theme={dark}>
+      <CalculatorDisplay value="0" />
+    </ThemeProvider>,
+  )
   expect(container.firstChild).toMatchInlineSnapshot(`
     .emotion-0 {
       position: relative;
@@ -13,6 +20,8 @@ test('render', () => {
       -webkit-flex: 1;
       -ms-flex: 1;
       flex: 1;
+      color: white;
+      background: #1c191c;
     }
 
     <div
@@ -20,6 +29,7 @@ test('render', () => {
     >
       <div
         class="autoScalingText"
+        data-testid="total"
         style="transform: scale(1,1);"
       >
         0
